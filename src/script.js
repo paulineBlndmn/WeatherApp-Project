6005c5443a40674f8e12882c8bb692bb178fa708
paulineBlndmn <paulineblindeman@gmail.com>
2021-12-12 19:59:58 +0800

    add the date and removed the time

--- src/script.js
+++ src/script.js
@@ -10,16 +10,25 @@ let days = [
   "Saturday",
 ];
 let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
 let displayedDate = document.querySelector("#date");
-displayedDate.innerHTML = `${day} ${hours}:${minutes}`;
+displayedDate.innerHTML = `${day} ${month} ${date} ${year}`;
 //
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
